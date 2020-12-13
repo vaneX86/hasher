b2aa5757b4f08b75252b740650f67414f727118c
vaneX86 <75688820+vaneX86@users.noreply.github.com>
2020-12-14 00:06:58 +0100

    Prefix detection

--- commands/help.js
+++ commands/help.js
@@ -3,16 +3,16 @@ var moment = require('moment');
 module.exports = {
     name: 'help',
     description: 'Help command! (cmds list)',
-    execute(message, Discord, client) {
+    execute(message, Discord, client, prefix) {
         const user = message.mentions.users.first() || message.member.user
         var dateNow = moment().format("DD-MM-YYYY hh:mm:ss");
         const help = new Discord.MessageEmbed()
         .setColor('#fc9803')
         .setAuthor(`Hasher Help Menu`, client.user.avatarURL())
         .setDescription([
-            `**;hash <algorithm> <text>** - Hash text command`,
-            `**;github** - Github repository URL`,
-            `**;help** - Help command`,
+            `**` + prefix + `hash <algorithm> <text>** - Hash text command`,
+            `**` + prefix + `github** - Github repository URL`,
+            `**` + prefix + `help** - Help command`,
         ])
         .setFooter(`Sended to: ${user.username} | Date: ` + dateNow)
         message.channel.send(help);
